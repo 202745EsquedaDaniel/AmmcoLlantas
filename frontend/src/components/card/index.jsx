6851a68881525bc129383053abee2d557a7d6a29
00lar <eduardani84@gmail.com>
2024-05-14 00:01:30 -0600

    cards fixed

--- frontend/src/components/card/index.jsx
+++ frontend/src/components/card/index.jsx
@@ -5,7 +5,7 @@ const Card = (data) => {
          >
             <figure className="relative mb-2 w-full h-4/5">
                 <span className="absolute bottom-0 left-0 bg-white/60 rounded-xl text-black text-xs m-2 px-3 py-0.6">{data.name}</span>
-                <img className="w-full h-full object-cover rounded-lg" src={""} alt="HeadPhone"></img>
+                <img className="w-full h-full object-cover rounded-lg" src={"https://e7.pngegg.com/pngimages/525/536/png-clipart-tires-tires.png"} alt="HeadPhone"></img>
             </figure>
             <p className="flex justify-between">
                 <span className="text-sm font-light">{data.data.name}</span>

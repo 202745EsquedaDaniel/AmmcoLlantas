cf6d44d81955c9ee465e922363bf4428b21fdb59
00lar <eduardani84@gmail.com>
2024-05-16 00:01:43 -0600

    add products ux/ui

--- frontend/src/pages/product/index.jsx
+++ frontend/src/pages/product/index.jsx
@@ -21,11 +21,30 @@ function Products(){
         
 
         <Layout>
-            <div className=" flex items-center justify-center w-80 relative">
-                <h1>Llantas</h1>
+            <div className=" flex relative">
+                <div className=" bg-white h-screen p-4">
+                    <h2 className=" text-lg">Agregar Nuevo Producto</h2>
+                    <div className=" border-y-2 pb-2">
+                        <p>PRODUCT INFO</p>
+                        <p className=" font-semibold">Marca</p>
+                        <input type="text" placeholder="Michelin"  className=" bg-grayInput rounded-xl p-2"/>
+                        <p className=" font-semibold mt-3">Modelo de llanta</p>
+                        <input type="text" placeholder="265/65/R16"  className=" bg-grayInput rounded-xl p-2"/>
+                        <p className=" font-semibold mt-3">Precio ($)</p>
+                        <input type="number" placeholder="1200"  className=" bg-grayInput rounded-xl p-2"/>
+                        <p className=" font-semibold mt-3">Stock</p>
+                        <input type="number" placeholder="4"  className=" bg-grayInput rounded-xl p-2"/>
+                    </div>
+                    <div>
+                        <button className=" bg-blue-500 text-white rounded-xl p-2 mt-4">Guardar</button>
+                    </div>
+                </div>
+                <div>
                 {
                     renderView()
                 }
+                </div>
+
             </div>
             
         </Layout>
